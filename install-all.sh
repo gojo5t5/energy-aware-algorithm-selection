17367d21b037229d3a77873312d3de1c50173ae4
gojo satoru <hematteo@outlook.com>
2023-02-06 13:03:01 +0000

    restructure

--- install-all.sh
+++ install-all.sh
@@ -2,7 +2,7 @@ BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 pushd $BIN_DIR
 
-for s in conjure savilerow mininzinc irace runsolver ortools yuck picat
+for s in mininzinc ortools yuck picat
 do
     bash install-${s}.sh
 done
