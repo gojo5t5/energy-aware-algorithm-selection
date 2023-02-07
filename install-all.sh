c6a04943894b469cdfc7bdf16342db75b30d19de
gojo satoru <hematteo@outlook.com>
2023-02-07 00:31:23 +0000

    add export for minizinc conmmand line

--- install-all.sh
+++ install-all.sh
@@ -2,9 +2,11 @@ BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 pushd $BIN_DIR
 
-for s in mininzinc ortools yuck picat
+for s in minizinc ortools yuck picat
 do
     bash install-${s}.sh
 done
+
+sudo apt install linux-tools-`uname -r`
 
 popd
--- run-experiments.sh
+++ run-experiments.sh
@@ -1,10 +1,16 @@
 # Should place all models in models/ and all instances in instances/
 # Run all experiments for all models, instances and solvers
-for m in models/*.dzn; do
-    for i in instances/*.dzn; do
+
+# move files into the right place
+
+# set path to use minizinc command
+
+for m in mznc2022_probs/*.mzn; do
+    for i in mznc2022_probs/*.{dzn,json}; do
         for s in solvers/*; do
-            perf record minizinc $m $i --solver $s -t 60
-            perf report --stdio > reports/$(basename $m)-$(basename $i)-$(basename $s).txt
+            # perf record minizinc $m $i --solver $s -t 60
+            sudo perf stat -e minizinc $m $i --solver $s -t 60
+            # perf report --stdio > reports/$(basename $m)-$(basename $i)-$(basename $s).txt
         done
     done
 done
